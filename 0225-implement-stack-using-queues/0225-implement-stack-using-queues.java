9890b6f6904b334a90e2c7f7f4c3330c3d8637de
Harish Prajapat <hk20032005@gmail.com>
2026-07-10 18:15:52 +0530

    Time: 2 ms (17.44%), Space: 42.9 MB (42.2%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -1,74 +1,27 @@
 class MyStack {
-    Queue<Integer> q1;
-    Queue<Integer> q2;
-
+    Queue<Integer> q;
     public MyStack() {
-        q1 = new LinkedList<>();
-        q2 = new LinkedList<>();
+        q=new LinkedList<>();
     }
-
+    
     public void push(int x) {
-        if (!q1.isEmpty()) {
-            q1.add(x);
-        } else {
-            q2.add(x);
+        q.add(x);
+
+        for(int i=0;i<q.size()-1;i++){
+            q.add(q.remove());
         }
     }
-
+    
     public int pop() {
-        if (empty())
-            return -1;
-
-        if (!q1.isEmpty()) {
-
-            while (q1.size() > 1) {
-                q2.offer(q1.poll());
-            }
-
-            return q1.poll();
-
-        } else {
-
-            while (q2.size() > 1) {
-                q1.offer(q2.poll());
-            }
-
-            return q2.poll();
-        }
+        return q.remove();
     }
-
+    
     public int top() {
-
-        if (empty())
-            return -1;
-
-        int ans;
-
-        if (!q1.isEmpty()) {
-
-            while (q1.size() > 1) {
-                q2.offer(q1.poll());
-            }
-
-            ans = q1.poll();
-            q2.offer(ans);
-
-        } else {
-
-            while (q2.size() > 1) {
-                q1.offer(q2.poll());
-            }
-
-            ans = q2.poll();
-            q1.offer(ans);
-        }
-
-        return ans;
-
+        return q.peek();
     }
-
+    
     public boolean empty() {
-        return q1.isEmpty() && q2.isEmpty();
+        return q.isEmpty();
     }
 }
 
