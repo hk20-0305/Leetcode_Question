c52731e1d2ad09faf6bc490e18cbdcdfc1d0b242
Harish Prajapat <hk20032005@gmail.com>
2026-07-13 23:01:20 +0530

    Time: 0 ms (100%), Space: 41.9 MB (86.86%) - LeetHub

---
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isStrictlyPalindromic(int n) {
+    //     for(int i=2;i<n-1;i++){
+    //         String num = cBs(n,i);
+    //          if(!isPalin(num)){
+    //         return false;
+    //     }
+    //     }
+    //     return true;
+      
+    // }
+    // public boolean isPalin(String s){
+
+    //    int i = 0, j = s.length() - 1;
+    //     while (i < j) {
+    //         if (s.charAt(i) != s.charAt(j)) return false;
+    //         i++;
+    //         j--;
+    //     }
+    //     return true;
+    // }
+
+    //  private String cBs(int n, int b) {
+    //     if (n == 0) return "0";
+    //     StringBuilder sb = new StringBuilder();
+    //     while (n > 0) {
+    //         int rem = n % b;
+    //         sb.append(rem);
+    //         n /= b;
+    //     }
+        // return sb.reverse().toString();
+        return false;
+    }
+}
