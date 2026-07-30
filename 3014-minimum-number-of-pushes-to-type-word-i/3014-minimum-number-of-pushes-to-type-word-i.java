3b6e886f67aebf1712b3bfd678431356723fee3a
Harish Prajapat <hk20032005@gmail.com>
2026-07-31 00:05:45 +0530

    Time: 0 ms (100%), Space: 42.8 MB (97.85%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minimumPushes(String word) {
+        
+        int n=word.length();
+
+        if(n<=8) return n;
+        int ans=0;
+        int i=1;
+        while(n>8){
+            ans+=8*i;
+            n=n-8;
+            i++;
+        }
+       ans+=n*i;
+       return ans;
+    }
+}
